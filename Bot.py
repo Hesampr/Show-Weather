8e52d56b85fbe45fd5d4e1ef67d4c128533b5cfa
Hesampr <hessam.pourrostami@gmail.com>
2023-06-03 21:48:02 +0330

    Finish off weather bot
    I add all of functions we need like show weather, chat handler and
    something like that and I thought I didn't need upperCase function so I
    deleted it.
    
    It's just a simple bot and I'll add some features soon.

--- Bot.py
+++ Bot.py
@@ -1,7 +1,11 @@
+# Some library we need to use 
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import WeatherClass 
 
+
+# Basic configs(don't change that)
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -14,39 +18,46 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # function for non-command replay
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="please write a command to use")
-
-# function for convert lower case sentences to upper case 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
+    
 # function for handler unknown command
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Wrong command")
 
 # help function
 async def help(update : Update, context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text= "/start : Starting bot\n/caps something : Convert sentence to upper case")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text= "/start : Starting bot\n/weather City : showing general and feelslike of today ")
 
+# Show weather 
+async def show_weather(update : Update, context : ContextTypes.DEFAULT_TYPE):
+    try : 
+        input_city = context.args[0]
+        general_weather, feelslike = WeatherClass.Weather(input_city)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text= f" Today's general weather : {general_weather}\nToday's feelslike : {feelslike} °C" )
+    except : 
+        pass
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('5514096270:AAFr30Wcg0MTYMeZ-FjKH4vTBZP0K-Y8Pb0').build()
-        
-    start_handler = CommandHandler('start', start)
+# Appliction builder 
+application = ApplicationBuilder().token('5514096270:AAFr30Wcg0MTYMeZ-FjKH4vTBZP0K-Y8Pb0').build()
 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+""" We made some handler for doing simple works 
+start and help handler is simple to know 
+echo handler works on client messages without command, it run help command to the client and after that client know the right commands """  
 
-    caps_handler = CommandHandler('caps',caps)
+start_handler = CommandHandler('start', start)
+
+echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), help)
    
-    help_handler = CommandHandler('help',help)
+help_handler = CommandHandler('help',help)
 
-    application.add_handler(start_handler)
+weather_handler = CommandHandler('weather',show_weather)
 
-    application.add_handler(echo_handler)
+application.add_handler(start_handler)
 
-    application.add_handler(caps_handler)
+application.add_handler(echo_handler)
 
-    application.add_handler(help_handler)
+application.add_handler(help_handler)
+
+application.add_handler(weather_handler)
  
         
-    application.run_polling()
+application.run_polling()
--- WeatherClass.py
+++ WeatherClass.py
@@ -1,25 +1,25 @@
-
+# A library we need to receive weather data 
 import requests
 
 
-
+# An API KEY I received from weatherstack.com (it's a free website)
 Api_Key = '4cd467403fcb8ed14a34dae13af4a37f'
+
 Base_Url = f"http://api.weatherstack.com/current?access_key={Api_Key}"
 
-def to_celsius(temperature):
-    return temperature - 273
 
-City = "ramsar"
-# City = input()
+# Weather receiver function 
+def Weather(city):
+    
+    Url = f"{Base_Url}&query={city}"
+    try : 
+        Response = requests.get(Url).json()
 
-Url = f"{Base_Url}&query={City}"
+        General_Weather = Response['current']['weather_descriptions']
+        
+        Today_feelslike = Response['current']['feelslike']
 
-Response = requests.get(Url).json()
+        return General_Weather[0],Today_feelslike
+    except: 
+        pass 
 
-General_Weather = Response['current']['weather_descriptions']
-Today_temperature = to_celsius(Response['current']['temperature'])
-Today_feelslike = Response['current']['feelslike']
-
-print(General_Weather)
-print(Today_temperature)
-print(Today_feelslike)
